f3077b0e912b5df37ba8bd3dabb32ddb45d7179a
vincent rouet <vincent.rouet@microsoft.com>
2024-03-12 12:00:24 +0000

    Add db project

--- code/sql-auth/grant-access.sql
+++ code/sql-auth/grant-access.sql
@@ -1,8 +1,4 @@
 -- Add a user, specifying the name in AD. When a user is added, it gets login access.
-CREATE USER logicproductcatalog FROM external provider
+CREATE USER [logicapps-vro] FROM external provider
 -- grant access to that user to execute stored proc
- grant execute on object::GetProducts to logicproductcatalog 
- 
- CREATE USER apps_productsread FROM external provider
- grant execute on object::GetProducts to apps_productsread 
-ALTER ROLE db_datareader remove MEMBER apps_productsread;
+ grant execute on object::GetProducts to [logicapps-vro] 
